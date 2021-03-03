b6050c52041bea3fffd84c8b723dfe23fc85c330
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-03-03 14:47:29 -0800

    added signin with auth

---
+++ src/components/welcome/welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class welcome extends Component {
+  render() {
+    return (
+      <div>
+                HELLLO PUDDY BUDDY COOKIE CRUMBLE HONEY. Eat a pie. 
+      </div>
+    );
+  }
+}
+
+export default welcome;
