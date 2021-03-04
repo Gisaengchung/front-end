bf748cdd50c02c1f8c3cfacec364b2cf0a2732f3
Sydney James <sydneyjames7@gmail.com>
2021-03-04 11:55:59 -0800

    added project form state and handle submit

--- src/components/moviePitchForm/MoviePitchForm.jsx
+++ src/components/moviePitchForm/MoviePitchForm.jsx
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createProject } from '';
 
-class AboutUs extends Component {
-  render() {
-    return (
-      <div>
-                
-      </div>
-    );
-  }
-}
+const ProjectForm = ({ index }) => { 
+  const dispatch = useDispatch();
 
-export default AboutUs;
+  const [projectTitle, setProjectTitle] = useState('');
+  const [projectSubtitle, setProjectSubtitle] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');  
+  const [projectGenre, setProjectGenre] = useState('');
+  const [projectState, setProjectState] = useState('');
+  const [projectCity, setProjectCity] = useState('');
+  const [projectMainImage, setProjectMainImage] = useState('');
+  const [projectFundingGoal, setProjectFundingGoal] = useState('');
+  const [projectFundingExDate, setProjectFundingExDate] = useState('');
+  const [projectRiskChallenge, setProjectRiskChallenge] = useState('');
+  const [projectDiversityCommit, setProjectDiversityCommit] = useState('');
+
+  const handleSubmitProject = event => {
+    event.preventDefault();
+    dispatch(createProject({ 
+      projectTitle, 
+      projectSubtitle, 
+      projectDescription,
+      projectGenre,
+      projectState,
+      projectCity,
+      projectMainImage,
+      projectFundingGoal,
+      projectFundingExDate,
+      projectRiskChallenge,
+      projectDiversityCommit, 
+      index }));  
+  };
+
+  return (
+    <form onSubmit={handleSubmitProject}>
+      <input
+        type="text"
+        placeholder="Project Title"
+        value={projectTitle}
+        onChange={({ target }) => setProjectTitle(target.value)}
+      />
+
+      <button>Create Project</button>
+    </form>  
+  );
+};
+  
+export default ProjectForm;
