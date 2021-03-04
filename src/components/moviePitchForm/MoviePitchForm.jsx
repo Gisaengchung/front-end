5eb050a9bce44ac56c1f00dfb31d608c5fbcc144
Sydney James <sydneyjames7@gmail.com>
2021-03-04 12:21:31 -0800

    form fixed

--- src/components/moviePitchForm/MoviePitchForm.jsx
+++ src/components/moviePitchForm/MoviePitchForm.jsx
@@ -9,13 +9,13 @@ const ProjectForm = ({ index }) => {
   const [projectSubtitle, setProjectSubtitle] = useState('');
   const [projectDescription, setProjectDescription] = useState('');  
   const [projectGenre, setProjectGenre] = useState('');
-  const [projectState, setProjectState] = useState('');
-  const [projectCity, setProjectCity] = useState('');
+  const [projectLocState, setProjectLocState] = useState('');
+  const [projectLocCity, setProjectLocCity] = useState('');
   const [projectMainImage, setProjectMainImage] = useState('');
   const [projectFundingGoal, setProjectFundingGoal] = useState('');
   const [projectFundingExDate, setProjectFundingExDate] = useState('');
   const [projectRiskChallenge, setProjectRiskChallenge] = useState('');
-  const [projectDiversityCommit, setProjectDiversityCommit] = useState('');
+  const [projectDiversity, setProjectDiversity] = useState('');
 
   const handleSubmitProject = event => {
     event.preventDefault();
@@ -24,13 +24,13 @@ const ProjectForm = ({ index }) => {
       projectSubtitle, 
       projectDescription,
       projectGenre,
-      projectState,
-      projectCity,
+      projectLocState,
+      projectLocCity,
       projectMainImage,
       projectFundingGoal,
       projectFundingExDate,
       projectRiskChallenge,
-      projectDiversityCommit, 
+      projectDiversity, 
       index }));  
   };
 
@@ -38,11 +38,70 @@ const ProjectForm = ({ index }) => {
     <form onSubmit={handleSubmitProject}>
       <input
         type="text"
-        placeholder="Project Title"
+        placeholder="Title"
         value={projectTitle}
         onChange={({ target }) => setProjectTitle(target.value)}
       />
-
+      <input
+        type="text"
+        placeholder="Subtitle"
+        value={projectSubtitle}
+        onChange={({ target }) => setProjectSubtitle(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Project Description"
+        value={projectDescription}
+        onChange={({ target }) => setProjectDescription(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Genre"
+        value={projectGenre}
+        onChange={({ target }) => setProjectGenre(target.value)}
+      />        
+      <input
+        type="text"
+        placeholder="State"
+        value={projectLocState}
+        onChange={({ target }) => setProjectLocState(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="City"
+        value={projectLocCity}
+        onChange={({ target }) => setProjectLocCity(target.value)}
+      />
+      <input
+        type="image"
+        placeholder="https://bit.ly/389vFsT"
+        value={projectMainImage}
+        onChange={({ target }) => setProjectMainImage(target.value)}
+      />
+      <input
+        type="number"
+        placeholder="5"
+        value={projectFundingGoal}
+        onChange={({ target }) => setProjectFundingGoal(target.value)}
+      />
+      <input
+        type="date"
+        placeholder="2030-01-01"
+        value={projectFundingExDate}
+        onChange={({ target }) => setProjectFundingExDate(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Risks and Challenges"
+        value={projectRiskChallenge}
+        onChange={({ target }) => setProjectRiskChallenge(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Risks and Challenges"
+        value={projectDiversity}
+        onChange={({ target }) => setProjectDiversity(target.value)}
+      />    
       <button>Create Project</button>
     </form>  
   );
