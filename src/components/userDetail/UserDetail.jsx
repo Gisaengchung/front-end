b9480d2879c8c9af76b417766840200c1756bd8e
Sydney James <sydneyjames7@gmail.com>
2021-03-05 18:20:10 -0800

    userslist attempt

--- src/components/userDetail/UserDetail.jsx
+++ src/components/userDetail/UserDetail.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'; 
 import { useSession } from '../../state/AuthUserProvider';
 import { NavLink } from 'react-router-dom';
--- src/services/fetches/fetches.js
+++ src/services/fetches/fetches.js
@@ -2,10 +2,12 @@ import { get, put } from '../request';
 
 export const getUserById = (id) => {
   return get(`user/${id}`);
-}
-;
+};
+
+export const getAllUsers = () => {
+  return get('user');
+};
 
 export const patchUserData = (userInfo) => {
   return put('user', userInfo);
-}
-;
+};
