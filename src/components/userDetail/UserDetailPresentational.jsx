11726399451241a1f4a900f4a82b8a114bd167ba
kevinfiero <kjf46@cornell.edu>
2021-03-07 01:43:58 -0800

    updated user workflow

--- src/components/userDetail/UserDetailPresentational.jsx
+++ src/components/userDetail/UserDetailPresentational.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const UserListPresentation = ({ user }) => {
-  console.log(user);
-
-
- 
+  
   return (
     <>
       <img src={user.profileImageUrl} />
