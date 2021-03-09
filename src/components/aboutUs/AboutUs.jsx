4eefc94fe338c63bcd55c214851581d78fefa7fe
Katelinn Jones <katiejonesyo@gmail.com>
2021-03-09 01:36:09 -0800

    final styling before adding about info

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -16,7 +16,7 @@ export default function AboutUs() {
         
         <div className={styles.picContainer}>
         <img className={styles.ktImage} src={rird} />
-        <h2>Richard</h2>
+        <h2 className={styles.aboutNames}>Richard</h2>
         <span>Hi! My Name is Richard. In the future time is backwards!</span>
         <p/>
         <a href="https://github.com/Richard-Hillman">
@@ -33,7 +33,7 @@ export default function AboutUs() {
         <div className={styles.picContainer}>
        
         <img className={styles.ktImage} src={kt} />
-        <h2>Kt</h2>
+        <h2 className={styles.aboutNames}>Kt</h2>
         <span>Hi! My name is kt!I am tall!!!!!!WOOOOO!!!</span>
         <p/>
         <a href="https://github.com/katiejonesyo">
@@ -48,7 +48,7 @@ export default function AboutUs() {
         
         <div className={styles.picContainer}>
         <img className={styles.ktImage} src={kevin} />
-        <h2>Kevin</h2>
+        <h2 className={styles.aboutNames}>Kevin</h2>
         <span>Hi! My name is Ferrari!I am Ferrari!</span>
         <p/>
         <a href="https://github.com/kevinfiero">
@@ -63,7 +63,7 @@ export default function AboutUs() {
 
         <div className={styles.picContainer}>
         <img className={styles.ktImage} src={sydney} />
-        <h2>Sydney</h2>
+        <h2 className={styles.aboutNames}>Sydney</h2>
         <span>Hi! My name is Sydney! I am not tall!</span> 
         <p/>
         <a href="https://github.com/scjam">
