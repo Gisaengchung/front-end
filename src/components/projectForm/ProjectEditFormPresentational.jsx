c5d3718b5d5a44787fc7cd786d4d75fe3728afdc
kevinfiero <kjf46@cornell.edu>
2021-03-08 22:29:24 -0800

    align project CREATE and EDIT form to require the same fields

--- src/components/projectForm/ProjectEditFormPresentational.jsx
+++ src/components/projectForm/ProjectEditFormPresentational.jsx
@@ -5,6 +5,7 @@ import { useHistory, useParams } from 'react-router';
 import { patchProjectData } from '../../services/fetches/fetches';
 import { useSession } from '../../state/AuthUserProvider';
 import styles from './ProjectForm.css';
+import UpLoader from '../cloudinary/UpLoader';
 
 const ProjectEditFormPresentational = ({ project }) => { 
 
@@ -56,6 +57,7 @@ const ProjectEditFormPresentational = ({ project }) => {
           placeholder="Title"
           value={projectTitle}
           onChange={({ target }) => setProjectTitle(target.value)}
+          required
         />
         <input
           type="text"
@@ -74,34 +76,34 @@ const ProjectEditFormPresentational = ({ project }) => {
           placeholder="Genre"
           value={projectGenre}
           onChange={({ target }) => setProjectGenre(target.value)}
+          required
         />        
         <input
           type="text"
           placeholder="State"
           value={projectLocState}
           onChange={({ target }) => setProjectLocState(target.value)}
+          required
         />
         <input
           type="text"
           placeholder="City"
           value={projectLocCity}
           onChange={({ target }) => setProjectLocCity(target.value)}
+          required
         />
-        <input
-          type="image"
-          placeholder="https://bit.ly/389vFsT"
-          value={projectMainImage}
-          onChange={({ target }) => setProjectMainImage(target.value)}
-        />
+        <div>
+          <UpLoader setProfileImageUrl={setProjectMainImage} />
+        </div>
         <input
           type="number"
-          placeholder="5"
+          placeholder="0"
           value={projectFundingGoal}
           onChange={({ target }) => setProjectFundingGoal(target.value)}
+          required
         />
         <input
           type="date"
-          placeholder="2030-01-01"
           value={projectFundingExDate}
           onChange={({ target }) => setProjectFundingExDate(target.value)}
         />
@@ -113,10 +115,10 @@ const ProjectEditFormPresentational = ({ project }) => {
         />
         <input
           type="text"
-          placeholder="Diversity Baby"
+          placeholder="Diversity"
           value={projectDiversity}
           onChange={({ target }) => setProjectDiversity(target.value)}
-        />    
+        /> 
         <button>Update Project</button>
       </form>
     </div>
