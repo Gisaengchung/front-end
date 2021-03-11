26d74fe422fe53ab12cf8e9be67d4384b1369185
Katelinn Jones <katiejonesyo@gmail.com>
2021-03-11 13:44:42 -0800

    user list with venmo icon

--- src/components/userDetail/UserDetailPresentational.jsx
+++ src/components/userDetail/UserDetailPresentational.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './UserDetail.css';
 import { Link } from 'react-router-dom';
+import appicon from './userAssests/appicon.png';
 
 const UserListPresentation = ({ user }) => {
   return (
@@ -10,11 +11,11 @@ const UserListPresentation = ({ user }) => {
       className={styles.userDetailPresentational}>
       <h2 className={styles.h2User}>{user.firstName} { user.lastName }</h2>
       <img className={styles.userImageDetail} src={user.profileImageUrl} />
-      <h5>{user.userCity}, {user.userState}</h5>
+      <h5 className={styles.location}>{user.userCity}, {user.userState}</h5>
       <div className={styles.userDetail}>{user.tagline}</div>
       <div className={styles.userDetail}>{user.userRole}</div>
       <div className={styles.userDetail}>{user.email}</div>
-      <div className={styles.userDetail}>{user.paymentHandle}</div>
+      <div className={styles.userDetail}><img className={styles.venmo} src={appicon} /> {user.paymentHandle}</div>
     </Link>
   );
 };
