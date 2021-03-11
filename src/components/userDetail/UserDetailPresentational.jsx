bef18135277a0da2f7f6fd1ad09e9ab20809a7cb
Sydney James <sydneyjames7@gmail.com>
2021-03-11 11:47:17 -0800

    more css edits

--- src/components/userDetail/UserDetailPresentational.jsx
+++ src/components/userDetail/UserDetailPresentational.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './UserDetail.css';
+import { Link } from 'react-router-dom';
 
 const UserListPresentation = ({ user }) => {
   return (
-    <div className={styles.userDetailPod}>
-      <div className={styles.userDetailPresentational}>
-        <h2 className={styles.h2User}>{user.firstName} { user.lastName }</h2>
-        <img className={styles.userImageDetail} src={user.profileImageUrl} />
-        <h5>{user.userCity}, {user.userState}</h5>
-        <div className={styles.userDetail}>{user.tagline}</div>
-        <div className={styles.userDetail}>{user.userRole}</div>
-        <div className={styles.userDetail}>{user.email}</div>
-        <div className={styles.userDetail}>{user.paymentHandle}</div>
-      </div>
-    </div>
+    <Link 
+      to={`/user-detail/${user.userId}`}  
+      className={styles.userDetailPresentational}>
+      <h2 className={styles.h2User}>{user.firstName} { user.lastName }</h2>
+      <img className={styles.userImageDetail} src={user.profileImageUrl} />
+      <h5>{user.userCity}, {user.userState}</h5>
+      <div className={styles.userDetail}>{user.tagline}</div>
+      <div className={styles.userDetail}>{user.userRole}</div>
+      <div className={styles.userDetail}>{user.email}</div>
+      <div className={styles.userDetail}>{user.paymentHandle}</div>
+    </Link>
   );
 };
 
