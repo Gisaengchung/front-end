e98907e8f91ccf349ef2739763bd7440349ed7bd
Katelinn Jones <70240110+katiejonesyo@users.noreply.github.com>
2021-03-10 18:01:18 -0800

    Update UserDetailPresentational.jsx

--- src/components/userDetail/UserDetailPresentational.jsx
+++ src/components/userDetail/UserDetailPresentational.jsx
@@ -6,15 +6,6 @@ const UserListPresentation = ({ user }) => {
   return (
     <div className={styles.userDetailPod}>
     <div className={styles.userDetailPresentational}>
-<<<<<<< HEAD
-      <img className={styles.userImageDetails} src={user.profileImageUrl} />
-      <h2 className={styles.h2User}>{ user.firstName } { user.lastName }</h2>
-      <h3>{ user.userCity }, { user.userState }</h3>
-      <div>{ user.tagline }</div>
-      <div>{ user.userRole }</div>
-      <div>{ user.email }</div>
-      <div>{ user.paymentHandle }</div>
-=======
       <h2 className={styles.h2User}>{user.firstName} { user.lastName }</h2>
       <img className={styles.userImageDetail} src={user.profileImageUrl} />
       <h5>{user.userCity}, {user.userState}</h5>
@@ -22,7 +13,6 @@ const UserListPresentation = ({ user }) => {
       <div className={styles.userDetail}>{user.userRole}</div>
       <div className={styles.userDetail}>{user.email}</div>
       <div className={styles.userDetail}>{user.paymentHandle}</div>
->>>>>>> 76019cb1caabd3caef4e3e48c836acfce707d9a9
     </div>
     </div>
   );
