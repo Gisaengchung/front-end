5fb3be714b8693b65eabd5a6d8517e00bf327f28
Katelinn Jones <katiejonesyo@gmail.com>
2021-03-10 16:12:22 -0800

    styling user detail

--- src/components/userDetail/UserDetailPresentational.jsx
+++ src/components/userDetail/UserDetailPresentational.jsx
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 import styles from './UserDetail.css'
 
 const UserListPresentation = ({ user }) => {
-  console.log(user);
   return (
+    <div className={styles.userDetailPod}>
     <div className={styles.userDetailPresentational}>
-      <img className={styles.userImageDetail} src={user.profileImageUrl} />
+      <img className={styles.userImageDetails} src={user.profileImageUrl} />
       <h2 className={styles.h2User}>{ user.firstName } { user.lastName }</h2>
       <h3>{ user.userCity }, { user.userState }</h3>
       <div>{ user.tagline }</div>
       <div>{ user.userRole }</div>
       <div>{ user.email }</div>
       <div>{ user.paymentHandle }</div>
+    </div>
     </div>
   );
 };
